904748724f1d852ced059043b9cb8ee77cba56e5
Nuwak <mr.igorr@gmail.com>
2016-11-06 18:45:44 +0300

    Добавил сообщение "Задание выполнено!"

--- js/main.js
+++ js/main.js
@@ -4,11 +4,15 @@
 $(document).ready(function () {
     $('.course__box--footer label').on('click', function (e) {
 
-        $(this).parent().parent().addClass('course__checked')
+        $(this).parent().parent().addClass('course__checked');
 
         var element = $(this);
         setTimeout(function () {
-            element.parent().parent().parent().hide()
+            element.parent().parent().parent().hide();
+
+            if($('.courses .course__box:visible').length == 0)
+                $('.finished').show();
+
         }, 1000);
     })
 });
